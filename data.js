1667d68f697b13eaadae8a5023e111a9bccc5bfd
wellmoredrone <85592388+wellmoredrone@users.noreply.github.com>
2024-06-11 14:10:05 -0400

    INIT

---
+++ data.js
@@ -0,0 +1,404 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pit-view-1-",
+      "name": "Pit View 1 ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.566321132485996,
+        "pitch": 0.9137594781734446,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5644501551888386,
+          "pitch": -0.03889674377838759,
+          "rotation": 0,
+          "target": "1-pit-view-2"
+        },
+        {
+          "yaw": 0.589983228508844,
+          "pitch": -0.004511549129869508,
+          "rotation": 0,
+          "target": "4-backstack-dump"
+        },
+        {
+          "yaw": -0.02898631179950506,
+          "pitch": -0.16188764125915966,
+          "rotation": 0,
+          "target": "2-drf-bc-"
+        },
+        {
+          "yaw": 0.06410513856219069,
+          "pitch": 0.05741736287905397,
+          "rotation": 0,
+          "target": "3-drf-c"
+        },
+        {
+          "yaw": -0.7523384401138387,
+          "pitch": 0.020222908115483662,
+          "rotation": 0,
+          "target": "5-top-of-drf-b"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.3054906868019778,
+          "pitch": 1.316060076596262,
+          "title": "Pit I-0603",
+          "text": "Clintwood B"
+        }
+      ]
+    },
+    {
+      "id": "1-pit-view-2",
+      "name": "Pit View 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.03142089157602257,
+        "pitch": 0.42465511884462437,
+        "fov": 0.873835604329209
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2649703232741665,
+          "pitch": 0.40118220347573086,
+          "rotation": 0,
+          "target": "0-pit-view-1-"
+        },
+        {
+          "yaw": 0.5269886043679186,
+          "pitch": 0.03899085860091844,
+          "rotation": 0,
+          "target": "4-backstack-dump"
+        },
+        {
+          "yaw": 0.08009543497978378,
+          "pitch": 0.010084192628857735,
+          "rotation": 0,
+          "target": "2-drf-bc-"
+        },
+        {
+          "yaw": 0.14862209374902235,
+          "pitch": 0.07094968466468643,
+          "rotation": 0,
+          "target": "3-drf-c"
+        },
+        {
+          "yaw": -0.253344386321654,
+          "pitch": 0.053444573941952456,
+          "rotation": 0,
+          "target": "5-top-of-drf-b"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.13537651538202056,
+          "pitch": 0.3163015443932231,
+          "title": "Pit I-0603",
+          "text": "Clintwood B"
+        }
+      ]
+    },
+    {
+      "id": "2-drf-bc-",
+      "name": "DRF-B&C ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.12189380452701393,
+        "pitch": 0.3159247936022993,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1287679631617955,
+          "pitch": 0.33101361134010077,
+          "rotation": 0,
+          "target": "3-drf-c"
+        },
+        {
+          "yaw": -0.8687106666976145,
+          "pitch": 0.016326938078131903,
+          "rotation": 0,
+          "target": "4-backstack-dump"
+        },
+        {
+          "yaw": -0.4450415783518231,
+          "pitch": -0.04124391434199204,
+          "rotation": 0,
+          "target": "0-pit-view-1-"
+        },
+        {
+          "yaw": 0.6526567683712692,
+          "pitch": -0.05521467384526346,
+          "rotation": 0,
+          "target": "5-top-of-drf-b"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.8046061477690394,
+          "pitch": 0.32489525680159304,
+          "title": "DRF-C",
+          "text": "~10M C.Y."
+        },
+        {
+          "yaw": 0.4784595956568509,
+          "pitch": 0.23066954307036092,
+          "title": "DRF-B",
+          "text": "~2.8M C.Y."
+        }
+      ]
+    },
+    {
+      "id": "3-drf-c",
+      "name": "DRF-C",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.02736641593071809,
+        "pitch": 0.6224470635854722,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.055475836043243,
+          "pitch": -0.08833930722659389,
+          "rotation": 0,
+          "target": "2-drf-bc-"
+        },
+        {
+          "yaw": -0.7625092607837569,
+          "pitch": 0.08602114479870693,
+          "rotation": 0,
+          "target": "4-backstack-dump"
+        },
+        {
+          "yaw": 1.583983698462113,
+          "pitch": -0.019615497878493926,
+          "rotation": 0,
+          "target": "5-top-of-drf-b"
+        },
+        {
+          "yaw": 0.1803527051889482,
+          "pitch": -0.06886007019043916,
+          "rotation": 0,
+          "target": "0-pit-view-1-"
+        },
+        {
+          "yaw": 0.034587586786319235,
+          "pitch": -0.062403069916223686,
+          "rotation": 0,
+          "target": "1-pit-view-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-backstack-dump",
+      "name": "Backstack Dump",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.08449609870766395,
+        "pitch": 0.6074108211227198,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0911959879976756,
+          "pitch": 0.287790246009509,
+          "rotation": 0,
+          "target": "3-drf-c"
+        },
+        {
+          "yaw": 2.046075186160871,
+          "pitch": -0.03416429811767152,
+          "rotation": 0,
+          "target": "2-drf-bc-"
+        },
+        {
+          "yaw": 0.8383404070329874,
+          "pitch": -0.03697406988692542,
+          "rotation": 0,
+          "target": "5-top-of-drf-b"
+        },
+        {
+          "yaw": -0.2792103820437841,
+          "pitch": -0.05898369859028918,
+          "rotation": 0,
+          "target": "0-pit-view-1-"
+        },
+        {
+          "yaw": -0.5610260310111101,
+          "pitch": -0.0323221541734533,
+          "rotation": 0,
+          "target": "1-pit-view-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-top-of-drf-b",
+      "name": "Top of DRF-B",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.4986643447037018,
+        "pitch": 0.5754344454898721,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7404194994736528,
+          "pitch": 0.1708394175497201,
+          "rotation": 0,
+          "target": "3-drf-c"
+        },
+        {
+          "yaw": -1.8768249055677515,
+          "pitch": 0.010067798272020312,
+          "rotation": 0,
+          "target": "2-drf-bc-"
+        },
+        {
+          "yaw": -1.320915924627048,
+          "pitch": -0.04866018855421217,
+          "rotation": 0,
+          "target": "4-backstack-dump"
+        },
+        {
+          "yaw": -0.44113179688146076,
+          "pitch": -0.043399816910760336,
+          "rotation": 0,
+          "target": "1-pit-view-2"
+        },
+        {
+          "yaw": -0.6153615869599172,
+          "pitch": -0.03819181659470594,
+          "rotation": 0,
+          "target": "0-pit-view-1-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Elk Creek 6-11-24",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
